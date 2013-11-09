e5400b871f877a62b8fe474ebad6a0847b5e0730
Jim Dalton <jim@ere.net>
2013-11-09 09:29:06 -0800

    Forgot routes for API

--- src/bababase/routes.clj
+++ src/bababase/routes.clj
@@ -1,13 +1,21 @@
 (ns bababase.routes
   (:require [caribou.app.pages :as pages]))
 
+(def api-routing
+  ["api/v1"    :api
+   [["states"  :api.states []]]])
+
 (def routes
-  [["/" :home []]])
+  [["/" :home [api-routing]]])
 
 (def pages
-  {:home {:GET {:controller 'home 
-                :action 'home 
-                :template "home.html"}}})
+  {:home       {:GET {:controller 'home 
+                      :action     'home 
+                      :template   "home.html"}}
+   :api        {:GET {:controller 'api
+                      :action     'index}}
+   :api.states {:GET {:controller 'api
+                      :action     'states-index}}})
 
 (defn page-tree
   []
